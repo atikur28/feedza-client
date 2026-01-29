9205940519713908e2d179602821a3dc7f1c50cf
atikur28 <atikur45683@gmail.com>
2026-01-29 13:05:42 +0600

    CategoryCards shown in homepage

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,3 +1,4 @@
+import CategoryCards from "@/components/homePage/CategoryCards";
 import HomeBanner from "@/components/homePage/HomeBanner";
 import HowItWorks from "@/components/homePage/HowItWorks";
 
@@ -6,6 +7,7 @@ export default function Home() {
     <div className="max-w-477 mx-auto">
       <HomeBanner />
       <HowItWorks />
+      <CategoryCards />
     </div>
   );
 }
