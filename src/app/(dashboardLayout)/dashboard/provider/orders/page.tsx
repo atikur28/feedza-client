8fd93ba0bc17b13989d633ad39ac3aece57541cb
atikur28 <atikur45683@gmail.com>
2026-01-30 03:35:31 +0600

    Provider Order page inserted

---
+++ src/app/(dashboardLayout)/dashboard/provider/orders/page.tsx
@@ -0,0 +1,11 @@
+import OrdersPageClient from "@/components/dashboard/providerDashboard/OrdersPageClient";
+
+const OrdersPage = async () => {
+  return (
+    <div>
+      <OrdersPageClient />
+    </div>
+  );
+};
+
+export default OrdersPage;
